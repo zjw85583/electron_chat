407cd3ff3d7fa257da67ce3836b5b6b17fc7ea24
zhujun <zjw85583@163.com>
2024-02-05 16:52:40 +0800

    1

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('message', (msg) => {
     console.log('user disconnected', msg);
+    /**
+     * socket.emit() 为发起者发送消息
+     * socket.broadcast.emit() 为除了发起者之外的人发送消息
+     * io.emit() 为所有人发送消息
+     */
     socket.emit('message', { ...msg, type: true });
     socket.broadcast.emit('dmessage', { ...msg, type: false });
   });
